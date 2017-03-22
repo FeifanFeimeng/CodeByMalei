8038b70f70588029e3d0adb57b67b97464a33520
FeifanFeimeng <ml1995.mail.ustc.edu.cn>
2017-03-22 18:52:08 +0800

    2017.3.22 exercise swiperFreshlayout

---
+++ swiperefreshlayout/src/main/java/com/shenzuo/malei/swiperefreshlayout/MainActivity.java
@@ -0,0 +1,41 @@
+package com.shenzuo.malei.swiperefreshlayout;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView)findViewById(R.id.textView1);
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_container);
+        //设置刷新时动画的颜色，可以设置4个
+        swipeRefreshLayout.setColorSchemeResources(android.R.color.holo_blue_light, android.R.color.holo_red_light, android.R.color.holo_orange_light, android.R.color.holo_green_light);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            @Override
+            public void onRefresh() {
+                tv.setText("正在刷新");
+                // TODO Auto-generated method stub
+                new Handler().postDelayed(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        tv.setText("刷新完成");
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 6000);
+            }
+        });
+    }
+}
